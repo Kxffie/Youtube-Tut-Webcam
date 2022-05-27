d4bd3bb91a3435eca8526702cb541c985d92caf6
Kxffie <78885838+Kxffie@users.noreply.github.com>
2022-05-26 22:50:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+##---
+# Kxffie
+# Created on : 5/26/22
+##---
+
 import cv2
 import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
@@ -15,33 +20,33 @@ maxThreshold = 1
 thresholdNormal = 0.5
 minThreshold = 0
 
-green = (0,255,0)
-blue = (255,0,0)
+green = (0, 255, 0)
+blue = (255, 0, 0)
 setColor = green
 
 while True:
     success, img = cap.read()
-    imgOut = segmentor.removeBG(img, (0, 255, 0), threshold=thresholdNormal)
+    imgOut = segmentor.removeBG(img, setColor, threshold=thresholdNormal)
     
     gray_scale = cv2.cvtColor(imgOut, cv2.COLOR_BGR2GRAY)
     faces = cascade.detectMultiScale(gray_scale)
     for (x, y, w, h) in faces:
         overlay_resize = cv2.resize(overlay, (w, 50))
-        imgOut = cvzone.overlayPNG(imgOut, overlay_resize, (x, y - 120))
+        imgOut = cvzone.overlayPNG(imgOut, overlay_resize, [x, y - 120])
     
-    cv2.imshow("Video", imgOut)
+    cv2.imshow("Image After", imgOut)
     key = cv2.waitKeyEx(1)
-    
-    if key == 2424832:
+    #! down key - 2621440
+    if key == 2424832: #! LEFT KEY
         if thresholdNormal > minThreshold:
             thresholdNormal -= 0.05
-    elif key == 2555904:
+    elif key == 2555904: #! RIGHT KEY
         if thresholdNormal < maxThreshold:
             thresholdNormal += 0.05
-    elif key == 2490368:
+    elif key == 2490368: #! UP KEY
         if setColor == green:
             setColor = blue
         else:
             setColor = green
-    elif key == 27:
+    elif key == 27: #! ESCAPE KEY
         break
